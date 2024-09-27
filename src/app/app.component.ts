929c2e12cbc08814b48a63fd6a9f1d6f1c28a0d0
khaled mohammed <dev.khaled.mohamed@gmail.com>
2024-09-27 23:42:13 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,40 +15,8 @@ export class AppComponent {
   constructor() {
 
   }
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
+ 
 
-  currentStatus = 'online';
-  // maxTrafficData = Math.max(...this.dummyTrafficData.map((data) =>   data.value));
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) =>   data.value));
 
 
 
--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -9,6 +9,6 @@ import { Component, Input } from '@angular/core';
 })
 export class ServerStatusComponent {
 
-  @Input() currentStatus:string = ''
+  currentStatus:string = 'online';
 
 }
