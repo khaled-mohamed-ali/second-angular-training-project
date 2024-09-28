fa09c6437666f1b4742db8d51d81bcc183eb58cf
khaled mohammed <dev.khaled.mohamed@gmail.com>
2024-09-28 22:02:41 +0300

    add new button component and update the style.css

--- src/app/dashboard/support-tickets/support-tickets.component.ts
+++ src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from "../tickets/new-ticket/new-ticket.component";
 
 @Component({
   selector: 'support-tickets',
   standalone: true,
-  imports: [],
+  imports: [NewTicketComponent],
   templateUrl: './support-tickets.component.html',
   styleUrl: './support-tickets.component.css'
 })
