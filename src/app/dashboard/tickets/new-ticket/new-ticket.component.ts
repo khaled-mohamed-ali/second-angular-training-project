804be644986f4473711cc96913c365851a24e3c8
khaled <dev.khlaed.mohamed@gmail.com>
2024-10-07 14:43:09 +0300

    fix bugs

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import { Component, output } from '@angular/core';
-=======
-import { afterNextRender, afterRender, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
->>>>>>> 23d843cdd734449be46721d7d415a969a81d3788
+import { afterNextRender, afterRender,output, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
-// import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-new-ticket',
@@ -16,11 +11,8 @@ import { ControlComponent } from "../../../shared/control/control.component";
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-<<<<<<< HEAD
   add = output<{title: string ; text: string}>()
-=======
 
->>>>>>> 23d843cdd734449be46721d7d415a969a81d3788
 
 
 onSubmit(title:string, textArea: string, form: HTMLFormElement) {
